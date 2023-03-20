1437d9477d0474da3faee8f2256f709f12434282
steven <steven.glikin@gmail.com>
2023-03-20 16:14:47 +0200

    progress on /health, database running

--- checkhealth.py
+++ checkhealth.py
@@ -1,12 +1,20 @@
 #!/bin/python3
 
 from flask import Flask
+import mysql.connector
+
 
 app = Flask(__name__)
 
 @app.route('/health')
 def health_check():
-    return 'OK', 200
-
+	try:
+		mydb = mysql.connector.connect(
+		host = "0fa8cfcbe42f",
+		user = "mysql",
+		password = "")
+		return 'OK', 200
+	except:
+		return 'failure', 500
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
