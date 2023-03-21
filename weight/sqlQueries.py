db8b6d44104a5d3ee2031cf1d441ebd0ec384532
Arnon Tzori <arnon44.at@gmail.com>
2023-03-21 13:40:23 +0200

    Added .env, started query implementation

--- weight/sqlQueries.py
+++ weight/sqlQueries.py
@@ -40,4 +40,18 @@ def get_container_weight_by_id(id: str):
 
 # For Carmen
 def get_transaction_range_by_dates_and_directions(start_date: str, end_date: str, directions: list[str]):
-    raise NotImplementedError
+    cnx = connect(**config)
+    if cnx.is_connected():
+        try:
+            cursor = cnx.cursor(dictionary=True)
+            query = (f"FROM transactions SELECT * WHERE truck_id = '{truck_id}'"
+                     "ORDER BY id DESC")
+            cursor.execute(query)
+            return cursor.fetchone()
+        except:
+            print("err")
+            # TODO: handle errors
+        finally:
+            if cnx.is_connected():
+                cursor.close()
+                cnx.close()
--- weight/weight.py
+++ weight/weight.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import datetime
 from flask_mysqldb import MySQL
 from http import HTTPStatus
-from os import environ
 
 app = Flask(__name__)
 
