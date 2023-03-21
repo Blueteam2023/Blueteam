b98dc550bc9dd63f1f2a13784239c0227aa194aa
steven <steven.glikin@gmail.com>
2023-03-21 11:15:26 +0200

    added  api stuff

--- billing/python/app.py
+++ billing/python/app.py
@@ -3,6 +3,7 @@
 #Importing modules
 from flask import Flask, request, render_template
 import mysql.connector
+import json
 
 #Initializing app
 app = Flask(__name__)
@@ -43,25 +44,33 @@ def plist():
 #POST provider API
 @app.route('/provider', methods=["GET", "POST"])
 def prov():
-	if request.method=="GET":
-		
+	if request.method=="GET":	
 		return render_template('index.html')
+		
 	elif request.method=="POST":	
-		try:
-			
-			connection=mysql.connector.connect(
-			user='root', password='root', host='mysql', port="3306", database='billdb')
-			provider_name = request.form['nm']
-			cursor=connection.cursor()
-			#cursor.execute("SELECT id FROM Provider where name=(%s)",(provider_name,))
-			cursor.execute("INSERT INTO Provider (`name`) VALUES ((%s));",(provider_name,))
-			cursor.execute("SELECT id FROM Provider where name=(%s)",(provider_name,))
-			provider=cursor.fetchall()
-							
-			connection.close()
-			return provider
-		except: 
-			return "Error: provider name already exists"
+		
+		GIVEN_PROV_NAME = request.form['nm']		
+		connection=mysql.connector.connect(
+		user='root', password='root', host='mysql', port="3306", database='billdb')
+		cursor=connection.cursor()
+
+
+		cursor.execute("SELECT name FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
+		DB_PROV_NAME=cursor.fetchall()	
+		json_NAME = json.loads(DB_PROV_NAME)
+		
+		if json_NAME == GIVEN_PROV_NAME:
+			cursor.execute("SELECT id FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
+			DB_PROV_ID=cursor.fetchall()
+			return "Error: provider name already exists at ID "
+		else:
+			cursor.execute("INSERT INTO Provider (`name`) VALUES ((%s));",(GIVEN_PROV_NAME,))	
+			cursor.execute("SELECT id FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
+			DB_PROV_ID=cursor.fetchall()			
+			return "else"
+		connection.close()
+
+
 	else:
 		return "error"
 
