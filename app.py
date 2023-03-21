a062b78425d8462189521f6ee1886f3b8ae25176
Eran <brylov@gmail.com>
2023-03-21 15:14:32 +0200

    bug fix #1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import re
 import subprocess
 
 
@@ -7,11 +8,9 @@ app = Flask(__name__)
 
 @app.route("/trigger", methods=['POST'])
 def trigger():
-  if request.method == 'POST':
+    if request.method == 'POST':
         event_type = request.headers.get('X-GitHub-Event')
-        print(request.headers)
         payload = request.get_json()
-        print(payload)
         if event_type == 'push':
             # handle push event
             branch = payload['ref'].split('/')[-1]
@@ -21,23 +20,19 @@ def trigger():
                 subprocess.run(['./scripts/build.sh', branch])
             elif branch =="devops":
                 subprocess.run(['./scripts/build.sh', branch])
-
-            return jsonify({"action": "", "pusher": pusher, "repository.branches_url": branch})
-          
-        elif event_type == 'pull_request':
-            action = payload['action']
-            if action == 'closed' and payload['pull_request']['merged'] and payload['pull_request']['base']['ref'] == 'main':
-                source_branch = payload['pull_request']['head']['ref']
+            elif branch =="main":
+                pattern = r'Merge pull request #\d+ from .*/(.*)'
+                source_branch = re.search(pattern, payload['head_commit']['message'])
                 print(source_branch)
             #    subprocess.run(['./scripts/production.sh', source_branch])
                 print("Bulding production")
-            
-        else:
-            # handle other event types
 
-            return 'OK', 200
-  else:
-    return 'Method not allowed', 405
+            return jsonify({"action": "", "pusher": pusher, "repository.branches_url": branch})
+        else:
+            return 'Unknown event', 400  
+
+    else:
+        return 'Method not allowed', 405
          
 
 
