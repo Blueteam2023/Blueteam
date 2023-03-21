42cb24cf7328f40f2eb65c6e6952824da31986e9
Arnon Tzori <arnon44.at@gmail.com>
2023-03-21 12:24:36 +0200

    Created get_container_weight_by_id

--- weight/sqlQueries.py
+++ weight/sqlQueries.py
@@ -11,6 +11,11 @@ def change_transaction(id: int, values: dict[str, Any]):
     raise NotImplementedError
 
 
+def get_container_weight_by_id(id: str):
+    raise NotImplementedError
+
 # For Carmen
+
+
 def get_transaction_range_by_dates_and_directions(start_date: str, end_date: str, directions: list[str]):
     raise NotImplementedError
