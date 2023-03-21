b09ef79bdb43f39c69c8f1f640a21859d1b7aff4
Arnon Tzori <arnon44.at@gmail.com>
2023-03-21 12:43:00 +0200

    Implemented get_last_transaction_by_truck

--- weight/sqlQueries.py
+++ weight/sqlQueries.py
@@ -13,7 +13,21 @@ config = {
 
 # For Yuval
 def get_last_transaction_by_truck(truck_id: str):
-    raise NotImplementedError
+    cnx = connect(**config)
+    if cnx.is_connected():
+        try:
+            cursor = cnx.cursor(dictionary=True)
+            query = (f"FROM transactions SELECT * WHERE truck_id = '{truck_id}'"
+                     "ORDER BY id DESC")
+            cursor.execute(query)
+            return cursor.fetchone()
+        except:
+            print("err")
+            # TODO: handle errors
+        finally:
+            if cnx.is_connected():
+                cursor.close()
+                cnx.close()
 
 
 def change_transaction(id: int, values: dict[str, Any]):
