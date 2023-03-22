d982b6b1fee1868f6e92036c959c655ffa92a2e1
Eran <brylov@gmail.com>
2023-03-22 19:23:21 +0200

    fix building

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,8 @@ if [ -z "$(docker ps -q -f name=weight-app)" ] || [ -z "$(docker ps -q -f name=b
     if [ -z "$(docker ps -q -f name=weight-app)" ]; then
         echo "Starting $team1 container"
         docker-compose -f /app/$team1/docker-compose.yaml up -d
-    elif [ -z "$(docker ps -q -f name=billing-app)" ]; then
+    fi
+    if [ -z "$(docker ps -q -f name=billing-app)" ]; then
         echo "Starting $team2 container"
         docker-compose -f /app/$team2/docker-compose.yaml up -d
     fi   
