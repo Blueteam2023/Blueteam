7d00bbb6ff66326146e379c84c2949dfeb378db4
Eran <brylov@gmail.com>
2023-03-22 19:27:23 +0200

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ team2="weight"
 
 if [ -z "$(docker ps -q -f name=weight-app)" ] || [ -z "$(docker ps -q -f name=billing-app)" ]; then
     echo "Running Production Envoirment"
-    if [ -z "$(docker ps -q -f name=weight-app)" ]; then
+    if [ -z "$(docker ps -q -f name=billing-app)" ]; then
         echo "Starting $team1 container"
         docker-compose -f /app/$team1/docker-compose.yaml up -d
     fi
-    if [ -z "$(docker ps -q -f name=billing-app)" ]; then
+    if [ -z "$(docker ps -q -f name=weight-app)" ]; then
         echo "Starting $team2 container"
         docker-compose -f /app/$team2/docker-compose.yaml up -d
     fi   
