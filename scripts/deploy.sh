dc32c7db377e0b9eaec89346add6d372d9da7d71
Eran <brylov@gmail.com>
2023-03-22 18:27:31 +0200

    fix premissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,11 @@ team1="billing"
 team2="weight"
 
 
-if [ -z $(docker ps | grep "weight-app") ] && [ -z $(docker ps | grep "billing-app") ]; then
+if [ -z "$(docker ps -q -f name=weight-app)" ] && [ -z "$(docker ps -q -f name=billing-app)" ]; then
     echo "Running Production Envoirment"
+    echo "Starting $team1 container"
     docker-compose -f /app/$team1/docker-compose.yaml up -d
+    echo "Starting $team2 container"
     docker-compose -f /app/$team2/docker-compose.yaml up -d
 else
     echo "Proudction containers already up"
