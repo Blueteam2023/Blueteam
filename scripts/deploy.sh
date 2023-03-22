b73c7297c5273dc3a83b94554b64d7fe89431ee4
Eran <brylov@gmail.com>
2023-03-22 19:06:33 +0200

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,15 @@ team1="billing"
 team2="weight"
 
 
-if [ -z "$(docker ps -q -f name=weight-app)" ] && [ -z "$(docker ps -q -f name=billing-app)" ]; then
+if [ -z "$(docker ps -q -f name=weight-app)" ] || [ -z "$(docker ps -q -f name=billing-app)" ]; then
     echo "Running Production Envoirment"
-    echo "Starting $team1 container"
-    docker-compose -f /app/$team1/docker-compose.yaml up -d
-    echo "Starting $team2 container"
-    docker-compose -f /app/$team2/docker-compose.yaml up -d
+    if [ -z "$(docker ps -q -f name=weight-app)" ]; then
+        echo "Starting $team1 container"
+        docker-compose -f /app/$team1/docker-compose.yaml up -d
+    elif [ -z "$(docker ps -q -f name=billing-app)" ]; then
+        echo "Starting $team2 container"
+        docker-compose -f /app/$team2/docker-compose.yaml up -d
+    fi   
 else
     echo "Proudction containers already up"
 fi
