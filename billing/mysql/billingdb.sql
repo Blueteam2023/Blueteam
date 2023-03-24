d838d733e4a6ca19fdcc5132028811656cc8e695
Yo Shoush <a0583245820@gmail.com>
2023-03-24 16:08:02 +0300

    get truck id to complete

--- billing/mysql/billingdb.sql
+++ billing/mysql/billingdb.sql
@@ -24,28 +24,56 @@ CREATE TABLE IF NOT EXISTS `Rates` (
   FOREIGN KEY (scope) REFERENCES `Provider`(`id`)
 ) ENGINE=MyISAM ;
 
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Navel', '50', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Blood', '50', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Mandarin', '50', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Shamuti', '50', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Tangerine', '50', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Clementine', '100', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Grapefruit', '100', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Valencia', '100', 'All');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Mandarin', '100', '43');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Mandarin', '100', '45');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Tangerine', '80', '12');
-INSERT INTO `Rates` (`product_id`, `rate`, `scope`) VALUES ('Valencia', '80', '45');
-
 CREATE TABLE IF NOT EXISTS `Trucks` (
   `id` varchar(10) NOT NULL,
   `provider_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`provider_id`) REFERENCES `Provider`(`id`)
 ) ENGINE=MyISAM ;
+
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('1010', '1');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('2020', '2');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('3030', '3');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('4040', '4');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('5050', '5');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('6060', '6');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('7070', '7');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('8080', '8');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('9090', '9');
+INSERT INTO `Trucks` (`id`, `provider_id`) VALUES ('100100', '10');
+
+
+
 --
 -- Dumping data
 --
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime DEFAULT NULL,
+  `direction` varchar(10) DEFAULT NULL,
+  `truck` varchar(50) DEFAULT NULL,
+  `containers` varchar(10000) DEFAULT NULL,
+  `bruto` int(12) DEFAULT NULL,
+  `truckTara` int(12) DEFAULT NULL,
+  --   "neto": <int> or "na" // na if some of containers unknown
+  `neto` int(12) DEFAULT NULL,
+  `produce` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10001 ;
+
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('11110', '20220101150000', 'in', '1010', '5', '2', '50', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('22220', '20220202150000', 'ni', '1010', '5', '2', '50', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('33330', '20220303150000', 'ni', '2020', '5', '2', '20', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('44440', '20220404150000', 'ni', '2020', '5', '2', '20', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('55550', '20220505150000', 'in', '2020', '5', '2', '20', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('66660', '20220606150000', 'ni', '2020', '5', '2', '20', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('77770', '20220707150000', 'ni', '100', '5', '2', '50', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('88880', '20220808150000', 'in', '100', '5', '2', '50', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('99990', '20220909150000', 'in', '100', '5', '2', '50', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('10101010', '20221010150000', 'in', '100', '5', '2', '50', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('11110', '20221111150000', 'in', '1010', '5', '2', '100', '74', '8');
+INSERT INTO `transactions` (`id`, `datetime`, `direction`, `trucks`, `containers`, `bruto`, `truckTara`, `neto`, `produce`) VALUES ('22220', '20221212150000', 'ni', '1010', '5', '2', '100', '74', '8');
+
 
 /*
 INSERT INTO Provider (`name`) VALUES ('ALL'), ('pro1'),
--- billing/python/app.py
+++ billing/python/app.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
-#Importing modules
-from flask import Flask, request, render_template
+# Importing modules
+from flask import Flask, request, render_template, abort, jsonify
 import mysql.connector
 import json
 import os
@@ -9,137 +9,148 @@ import pandas as pd
 from os import environ
 from flask import send_file
 from io import BytesIO
+from datetime import datetime, timedelta
 
-
-#Preparing environment variables
+# Preparing environment variables
 MYSQL_USER = environ['ENV_USER']
 MYSQL_ROOT_PASSWORD = environ['ENV_ROOT_PASSWORD']
 MYSQL_HOST = environ['ENV_HOST']
 MYSQL_DB_NAME = environ['ENV_DB_NAME']
 
-#Initializing app
+# Initializing app
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
 	return "Connected"
 
 
-#Health check API
+# Health check API
 @app.route('/health')
 def health_check():
 	try:
-		connection=mysql.connector.connect(
-		user = MYSQL_USER, password = MYSQL_ROOT_PASSWORD, host = MYSQL_HOST, port = "3306", database = MYSQL_DB_NAME)
+		connection = mysql.connector.connect(
+		user=MYSQL_USER, password=MYSQL_ROOT_PASSWORD, host=MYSQL_HOST, port="3306", database=MYSQL_DB_NAME)
 		connection.close()
 		return 'OK', 200
 	except:
 		return 'failure', 500
 
 
-#Provider list API
+# Provider list API
 @app.route('/providerlist')
 def plist():
 	try:
-		connection=mysql.connector.connect(
-		user = MYSQL_USER, password = MYSQL_ROOT_PASSWORD, host = MYSQL_HOST, port = "3306", database = MYSQL_DB_NAME)
+		connection = mysql.connector.connect(
+		user=MYSQL_USER, password=MYSQL_ROOT_PASSWORD, host=MYSQL_HOST, port="3306", database=MYSQL_DB_NAME)
 
-		cursor=connection.cursor()
+		cursor = connection.cursor()
 		cursor.execute('SELECT * FROM Provider;')
-		DB_PROV_LIST=cursor.fetchall()
-		#JSON_PROV_LIST=list(json.dumps(DB_PROV_LIST))
+		DB_PROV_LIST = cursor.fetchall()
+		# JSON_PROV_LIST=list(json.dumps(DB_PROV_LIST))
 		connection.close()
 		return DB_PROV_LIST
 	except:
 		return "listfail"
-	
-#POST provider API
+
+# POST provider API
+
+
 @app.route('/provider', methods=["GET", "POST"])
 def prov():
-	if request.method=="GET":	
+	if request.method == "GET":
 		return render_template('prov.html')
-		
-	elif request.method=="POST":	
-		
-		GIVEN_PROV_NAME = request.form['nm']		
-		connection=mysql.connector.connect(
-		user = MYSQL_USER, password = MYSQL_ROOT_PASSWORD, host = MYSQL_HOST, port = "3306", database = MYSQL_DB_NAME)
-		cursor=connection.cursor()
 
+	elif request.method == "POST":
 
-		cursor.execute("SELECT name FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
-		DB_PROV_NAME=cursor.fetchall()
+		GIVEN_PROV_NAME = request.form['nm']
+		connection = mysql.connector.connect(
+		user=MYSQL_USER, password=MYSQL_ROOT_PASSWORD, host=MYSQL_HOST, port="3306", database=MYSQL_DB_NAME)
+		cursor = connection.cursor()
+
+		cursor.execute("SELECT name FROM Provider where name=(%s)",
+		               (GIVEN_PROV_NAME,))
+		DB_PROV_NAME = cursor.fetchall()
 		PROV_NAME = json.dumps(DB_PROV_NAME)
-		
+
 		if PROV_NAME == f'[["{GIVEN_PROV_NAME}"]]':
-			cursor.execute("SELECT id FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
-			DB_PROV_ID=cursor.fetchall()
-			PROV_ID=json.dumps(DB_PROV_ID[0][0])
+			cursor.execute("SELECT id FROM Provider where name=(%s)",
+			               (GIVEN_PROV_NAME,))
+			DB_PROV_ID = cursor.fetchall()
+			PROV_ID = json.dumps(DB_PROV_ID[0][0])
 			connection.close()
 			return f"Error: provider name already exists at ID {PROV_ID}"
 		else:
-			cursor.execute("INSERT INTO Provider (`name`) VALUES ((%s));",(GIVEN_PROV_NAME,))	
-			cursor.execute("SELECT id FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
-			DB_PROV_ID=cursor.fetchall()
-			PROV_ID=json.dumps(DB_PROV_ID[0][0])
+			cursor.execute("INSERT INTO Provider (`name`) VALUES ((%s));",
+			               (GIVEN_PROV_NAME,))
+			cursor.execute("SELECT id FROM Provider where name=(%s)",
+			               (GIVEN_PROV_NAME,))
+			DB_PROV_ID = cursor.fetchall()
+			PROV_ID = json.dumps(DB_PROV_ID[0][0])
 			connection.close()
-			return f"Provider name saved to ID {PROV_ID}"		
+			return f"Provider name saved to ID {PROV_ID}"
 	else:
 		return "error"
 
 
-#PUT provider id API
+# PUT provider id API
 @app.route('/provider/<id>', methods=["GET", "POST"])
 def provid(id):
 
-	if request.method=="GET":	
+	if request.method == "GET":
 		return render_template('prov-id.html')
-		
-	elif request.method=="POST":	
-		
-		GIVEN_PROV_NAME = request.form['nm']		
-		connection=mysql.connector.connect(
-		user = MYSQL_USER, password = MYSQL_ROOT_PASSWORD, host = MYSQL_HOST, port = "3306", database = MYSQL_DB_NAME)
-		cursor=connection.cursor()
 
-		cursor.execute("SELECT name FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
-		DB_PROV_NAME=cursor.fetchall()
+	elif request.method == "POST":
+
+		GIVEN_PROV_NAME = request.form['nm']
+		connection = mysql.connector.connect(
+		user=MYSQL_USER, password=MYSQL_ROOT_PASSWORD, host=MYSQL_HOST, port="3306", database=MYSQL_DB_NAME)
+		cursor = connection.cursor()
+
+		cursor.execute("SELECT name FROM Provider where name=(%s)",
+		               (GIVEN_PROV_NAME,))
+		DB_PROV_NAME = cursor.fetchall()
 		PROV_NAME = json.dumps(DB_PROV_NAME)
-		
+
 		if PROV_NAME == f'[["{GIVEN_PROV_NAME}"]]':
-			cursor.execute("SELECT id FROM Provider where name=(%s)",(GIVEN_PROV_NAME,))
-			DB_PROV_ID=cursor.fetchall()
-			PROV_ID=json.dumps(DB_PROV_ID[0][0])
+			cursor.execute("SELECT id FROM Provider where name=(%s)",
+			               (GIVEN_PROV_NAME,))
+			DB_PROV_ID = cursor.fetchall()
+			PROV_ID = json.dumps(DB_PROV_ID[0][0])
 			connection.close()
 			return f"Error: provider name already exists at ID {PROV_ID}"
 		elif GIVEN_PROV_NAME == "":
-			cursor.execute("DELETE FROM `Provider` WHERE id=(%s);",(id,))
+			cursor.execute("DELETE FROM `Provider` WHERE id=(%s);", (id,))
 			connection.close()
-			return f"Provider deleted from database, id {id} is no longer in use" 
-			
-		else: 
-			cursor.execute("UPDATE Provider set name=(%s) where id=(%s)",(GIVEN_PROV_NAME, id,))
+			return f"Provider deleted from database, id {id} is no longer in use"
+
+		else:
+			cursor.execute("UPDATE Provider set name=(%s) where id=(%s)",
+			               (GIVEN_PROV_NAME, id,))
 			connection.close()
-			return f"New provider name saved to ID {id}"	
+			return f"New provider name saved to ID {id}"
 	else:
 		return "error"
+
 
 @app.route('/rates', methods=["GET", "POST"])
 def rates():
     if request.method == "POST":
         # Get the option selected by the user
         option = request.form['option']
-        
+
         if option == 'upload':
-        
+
             # Retrieve the tariff file from the request form
             rate_file = request.files['file'].read()
             excel_file = BytesIO(rate_file)
             df = pd.read_excel(excel_file)
-            
+
             required_columns = ['product_id', 'rate', 'scope']
             if not all(col in df.columns for col in required_columns):
-                 raise ValueError(f"Error: the DataFrame must have the columns {required_columns}!")
+                 raise ValueError(
+                     f"Error: the DataFrame must have the columns {required_columns}!")
 
 			# Save the new Excel file to the "/in" directory
             excel_data = BytesIO()
@@ -149,8 +160,6 @@ def rates():
 
             excel_file.close()
             excel_data.close()
-
-
 
             # Connect to the MySQL database
             connection = mysql.connector.connect(
@@ -165,7 +174,8 @@ def rates():
             cursor = connection.cursor()
             cursor.execute("TRUNCATE TABLE Rates")
             for row in df.itertuples():
-                cursor.execute(f"INSERT INTO Rates (product_id, rate, scope) VALUES ('{row.product_id}', '{row.rate}', '{row.scope}')")
+                cursor.execute(
+                    f"INSERT INTO Rates (product_id, rate, scope) VALUES ('{row.product_id}', '{row.rate}', '{row.scope}')")
             connection.commit()
             cursor.close()
             connection.close()
@@ -178,6 +188,74 @@ def rates():
 
     return render_template('rates.html')
 
-#main
+
+
+@app.route('/truck', methods=["GET", "POST"])
+def truck_info():
+	if request.method == "POST":	
+		try:
+			#from_param = request.args.get('from')
+			#to_param = request.args.get('to')
+
+			#if from_param is None:
+			from_param = request.form['from']
+			#if to_param is None:
+			to_param = request.form['to']
+
+			if from_param == "1st of month at 000000":
+				now = datetime.now()
+				from_param = datetime(now.year, now.month, 1)
+				from_param = from_param.strftime("%Y%m%d%H%M%S")
+			else:
+				from_param = datetime.strptime(from_param, "%Y%m%d%H%M%S")
+
+			if to_param == "now":
+				to_param = datetime.now()
+				to_param = to_param.strftime("%Y%m%d%H%M%S")
+			else:
+				to_param = datetime.strptime(to_param, "%Y%m%d%H%M%S")
+		except:
+			return "error parameters"
+		
+
+		connection = mysql.connector.connect(
+			user=MYSQL_USER,
+			password=MYSQL_ROOT_PASSWORD,
+			host=MYSQL_HOST,
+			port="3306",
+			database=MYSQL_DB_NAME
+		)
+		cursor = connection.cursor()
+		cursor.execute("SELECT * FROM Trucks WHERE id = %s", (id,))
+		result = cursor.fetchone()
+		if result:
+			date_now = datetime.now()
+			date_time = date_now.strftime('%Y%m%d%H%M%S')
+			# database weight connection
+			# connection = mysql.connector.connect(
+			# user=MYSQL_USER,
+			# password=MYSQL_ROOT_PASSWORD,
+			# host=MYSQL_HOST,
+			# port="3306",
+			# database=MYSQL_DB_NAME
+			# )
+			request1 = "SELECT truckTara FROM transactions WHERE truck = %s ORDER BY ABS(TIMEDIFF(datetime, %s)) LIMIT 1"
+			cursor.execute(request1, (id, date_time))
+			tara = cursor.fetchone()
+			request2 = "SELECT id FROM transactions WHERE datetime BETWEEN %s AND %s AND truck = %s"
+			cursor.execute(request2, (from_param, to_param, id))
+			sessions = set(cursor.fetchall())
+			cursor.close()
+			connection.close()
+			return jsonify({"id": id, "tara": tara, "sessions": sessions})
+		else:
+			abort(404)
+    
+    # renvoyer la page HTML
+	return render_template('datruckid.html')
+
+# main
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
+
+
