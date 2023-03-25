fa9929955dd2d2d8828a0096a40187aaf9076992
Eran <brylov@gmail.com>
2023-03-25 11:16:46 +0300

    bug fix

--- app.py
+++ app.py
@@ -104,6 +104,7 @@ def monitor():
 @app.route('/rollback', methods=['POST'])
 def rollback():
     tag = request.form['tag']
+    print(tag)
     t = threading.Thread(target=run_rollback, args=(tag))
     t.start()
     return redirect(url_for('monitor'), last_version="Rolling back in process")
